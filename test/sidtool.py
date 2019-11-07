dbf5fc1135762cedb3cfb1cf90562f41914fc501
stein.pedersen <stein.pedersen@bluware.com>
2019-11-07 19:17:28 +0100

    handle header version > 1

--- test/sidtool.py
+++ test/sidtool.py
@@ -1,27 +1,34 @@
+from __future__ import print_function
 import os
 import sys
 import numpy as np
 
 psid_fields = [
-  ('id', 'S4'),
-  ('version', '>u2'),
-  ('dataOffset', '>u2'),
+  ('id',          'S4' ),
+  ('version',     '>u2'),
+  ('dataOffset',  '>u2'),
   ('loadAddress', '>u2'),
   ('initAddress', '>u2'),
   ('playAddress', '>u2'),
-  ('songs', '>u2'),
-  ('startSong', '>u2'),
-  ('speed', '>u4'),
-  ('name', 'S32'),
-  ('author', 'S32'),
-  ('released', 'S32') 
+  ('songs',       '>u2'),
+  ('startSong',   '>u2'),
+  ('speed',       '>u4'),
+  ('name',        'S32'),
+  ('author',      'S32'),
+  ('released',    'S32') 
   ]
 
-rsid_fields = [ ]  
+psid2_fields = list(psid_fields)
+psid2_fields.extend([
+  ('flags',             '>u2'),
+  ('startPage',         'u1' ),
+  ('pageLength',        'u1' ),
+  ('secondSIDAddress',  'u1' ),
+  ('thirdSIDAddress',   'u1' )
+  ])
   
 psid_hdr_type = np.dtype(psid_fields)
-rsid_hdr_type = list(psid_fields)
-rsid_hdr_type.extend(rsid_fields)
+psid2_hdr_type = np.dtype(psid2_fields)
 
 def set_subtunes(contents, count):
     contents[0x0F] = int(count)
@@ -34,7 +41,16 @@ options = { \
 def load_sid(filename):
     raw = np.fromfile(filename, dtype=np.uint8)
     hdr = raw[0:psid_hdr_type.itemsize].view(dtype=psid_hdr_type)
-    return raw, hdr
+    if hdr['id'] == 'PSID':
+        if hdr['version'] > 1:
+            hdr = raw[0:psid2_hdr_type.itemsize].view(dtype=psid2_hdr_type)
+        return raw, hdr
+    elif hdr['id'] == 'RSID':
+#        hdr = raw[0:rsid_hdr_type.itemsize].view(dtype=rsid_hdr_type)
+#        return raw, hdr
+        raise TypeError("RSID format currently not supported : '{}'".format(filename))
+    else:
+        raise TypeError("Unknown SID format in file '{}'".format(filename))
 
 def main():
     apply_opts = []
