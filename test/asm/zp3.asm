aeda7aff18c8d61e5e0716a1301d4d3c52f12234
stein.pedersen <stein.pedersen@bluware.com>
2020-01-30 00:07:02 +0100

    fixed zp3 test so it doesn't crash in ram-only environment

--- test/asm/zp3.asm
+++ test/asm/zp3.asm
@@ -3,127 +3,140 @@
 
 mode = 0
 test = 0
+multi = 0
 
-	jmp init
+  jmp init
 dowork:
-	lda word_ready
-	bne read_next_char
-	ldx sentence_pos
+  lda word_ready
+  bne read_next_char
+  ldx sentence_pos
 readaddr:
-	lda $ffff,x
-	cmp #$ff
-	bne sentence_not_done
-	lda #1
-	sta is_done
-	rts
+  lda $ffff,x
+  cmp #$ff
+  bne sentence_not_done
+  lda #1
+  sta is_done
+  rts
 sentence_not_done:
-	asl
-	tax
-	lda wordlist, x
-	sta current_word
-	lda wordlist+1, x
-	sta current_word+1
-	inc word_ready
+  asl
+  tax
+  lda wordlist, x
+  sta current_word
+  lda wordlist+1, x
+  sta current_word+1
+  inc word_ready
 read_next_char:
 .if mode == 0
-	lda current_word
-	sta $fb
-	lda current_word+1
-	sta $fc
-    ldy #0	
+  lda current_word
+  sta $fb
+  lda current_word+1
+  sta $fc
+  ldy #0  
 .else
-	ldy current_word
-	lda current_word+1
-	sta $fc
-.endif	
-	lda ($fb), y
-	bne char_ok
-	lda #0
-	sta word_ready
-	inc sentence_pos
-	jmp dowork
+  ldy current_word
+  lda current_word+1
+  sta $fc
+.endif  
+  lda ($fb), y
+  bne char_ok
+  lda #0
+  sta word_ready
+  inc sentence_pos
+  jmp dowork
 char_ok:
-	jsr $ffd2
-	clc
-	lda current_word
-	adc #1
-	sta current_word
-	lda current_word+1
-	adc #0
-	sta current_word+1
-	rts
-	
+.if test
+  jsr $ffd2
+.else
+  nop
+.endif
+  clc
+  lda current_word
+  adc #1
+  sta current_word
+  lda current_word+1
+  adc #0
+  sta current_word+1
+  rts
+  
 init:
-	asl
-	tax
-	lda sentences,x
-	sta readaddr+1
-	lda sentences+1,x
-	sta readaddr+2
-	lda #0
-	sta $fb
-	sta sentence_pos
-	sta word_ready
-	sta is_done
-.if test	
+  asl
+  tax
+  lda sentences,x
+  sta readaddr+1
+  lda sentences+1,x
+  sta readaddr+2
+  lda #0
+  sta $fb
+  sta sentence_pos
+  sta word_ready
+  sta is_done
+.if test  
 loop:
-	lda is_done
-	bne done
-	jsr dowork
-	jmp loop
+  lda is_done
+  bne done
+  jsr dowork
+  jmp loop
 done:
 .endif
-	rts
+  rts
 
 is_done:
-	.byte 0
+  .byte 0
 sentence_pos:
-	.byte 0
+  .byte 0
 word_ready:
-	.byte 0
+  .byte 0
 current_word:
-	.word 0
-	
+  .word 0
+  
 wordlist:
-	.word space
-	.word punct
-	.word comma
-	.word excl
-	.word w1, w2, w3, w4, w5, w6, w7, w8
-		
+  .word space
+  .word punct
+  .word comma
+  .word excl
+  .word w1, w2, w3, w4, w5, w6
+.if multi
+  .word w7, w8
+.endif
+
 sentence1:
-	.byte 4, 0, 5, 1, 6, 0, 7, 0, 8, 2, 6, 0, 9, 3
-	.byte $ff
+  .byte 4, 0, 5, 1, 6, 0, 7, 0, 8, 2, 6, 0, 9, 3
+  .byte $ff
+.if multi
 sentence2:
-	.byte 10, 2, 11, 3
-	.byte $ff
+  .byte 10, 2, 11, 3
+  .byte $ff
+.endif
 
 sentences:
-	.word sentence1, sentence2
-	
+  .word sentence1
+.if multi
+  .word sentence2
+.endif
+  
 space:
-	.byte " ", 0
+  .byte " ", 0
 punct:
-	.byte ". ", 0
+  .byte ". ", 0
 comma:
-	.byte ", ", 0
+  .byte ", ", 0
 excl:
-	.byte "! ", 0
+  .byte "! ", 0
 w1:
-	.byte "ANOTHER", 0
+  .byte "ANOTHER", 0
 w2:
-	.byte "VISITOR", 0
+  .byte "VISITOR", 0
 w3:
-	.byte "STAY", 0
+  .byte "STAY", 0
 w4:
-	.byte "A", 0
+  .byte "A", 0
 w5:
-	.byte "WHILE", 0
+  .byte "WHILE", 0
 w6:
-	.byte "FOREVER", 0
+  .byte "FOREVER", 0
+.if multi  
 w7:
-	.byte "HELLO", 0
+  .byte "HELLO", 0
 w8:
-	.byte "WORLD", 0
-
-	
+  .byte "WORLD", 0
+.endif
